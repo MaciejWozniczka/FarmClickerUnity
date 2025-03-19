62ca2847bf2b58dee3951b90d8a745ecda64d239
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2025-03-19 16:48:39 +0100

    Added AssetPreviewer script

---
+++ Assets/Scripts/AssetPreviewer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ClickItem))]
+public class AssetPreviewer : Editor
+{
+    ClickItem item;
+    Texture2D tex1;
+    Texture2D tex2;
+
+    public override void OnInspectorGUI()
+    {
+        item = (ClickItem)target;
+
+        GUILayout.BeginHorizontal();
+
+        tex1 = AssetPreview.GetAssetPreview(item.ItemImage);
+        GUILayout.Label(tex1);
+
+        tex2 = AssetPreview.GetAssetPreview(item.ItemImage);
+        GUILayout.Label(tex2);
+
+        GUILayout.EndHorizontal();
+
+        DrawDefaultInspector();
+    }
+}
