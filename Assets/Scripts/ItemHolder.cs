ac63d3374f6609a8e8fc63a19722bac6dad6d725
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2025-03-19 23:43:21 +0100

    Added ItemHolder script

---
+++ Assets/Scripts/ItemHolder.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemHolder : MonoBehaviour
+{
+    public Image itemImage;
+    //public BuyButton BuyButton;
+    public Text ItemNameText;
+    public Text IncomeText;
+    public Text AmountText;
+    public Text CostText;
+}
