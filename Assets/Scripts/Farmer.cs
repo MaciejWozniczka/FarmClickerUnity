9bd9664850a2325240f1ad5f92de6102fba996a6
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2025-03-20 12:06:57 +0100

    Added Farmer Click service

--- Assets/Scripts/Farmer.cs
+++ Assets/Scripts/Farmer.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class Farmer : MonoBehaviour
 {
     int clickAmount = 1;
+
+    Animator anim;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     public void Click()
     {
         GameManager.instance.AddMoney(clickAmount);
+        anim.SetTrigger("Click");
     }
 }
