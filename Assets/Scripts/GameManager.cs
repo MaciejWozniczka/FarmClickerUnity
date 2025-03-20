d251efa5827594cb2c0549e7f3d46e22c52ffe58
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2025-03-20 10:13:46 +0100

    Updated GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    // Items
     [System.Serializable]
     public class AnyItem
     {
@@ -13,6 +15,17 @@ public class GameManager : MonoBehaviour
         [HideInInspector] public bool Created;
         [HideInInspector] public ItemHolder Holder;
     }
-
+    [Header("ITEMS")]
     public List<AnyItem> ItemList = new List<AnyItem>();
+
+    // Money
+    [Header("MONEY")]
+    public float Money;
+    public Text TotalMoneyText;
+    public Text TotalIncomeText;
+
+    // User Interface
+    [Header("USER INTERFACE")]
+    public GameObject ItemHolder;
+    public Transform Grid;
 }
