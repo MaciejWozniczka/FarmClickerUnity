771f08ceaa5a0f96406e61b837196739fe696855
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2025-04-16 00:47:35 +0200

    Added DeactivateText script

---
+++ Assets/Scripts/DeactivateText.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateText : MonoBehaviour
+{
+    Animator anim;
+    void OnEnable()
+    {
+        anim = GetComponent<Animator>();
+        AnimatorClipInfo[] info = anim.GetCurrentAnimatorClipInfo(0);
+        Invoke("Deactivate", info[0].clip.length);   
+    }
+
+    void Deactivate()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
